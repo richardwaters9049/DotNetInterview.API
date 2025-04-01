43ec7b8244fcbc9bf770e024b246b7f62367674d
Richy Rose <richardwaters866@gmail.com>
2025-04-01 14:04:53 +0100

    first commit

---
+++ Domain/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetInterview.API.Domain;
+
+public record Item
+{
+    public Guid Id { get; set; }
+    public required string Reference { get; set; }
+    public required string Name { get; set; }
+    public decimal Price { get; set; }
+    public ICollection<Variation> Variations { get; set; } = new List<Variation>();
+}
